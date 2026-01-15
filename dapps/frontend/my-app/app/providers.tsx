409c9605be26281e35348506fb59d6871d7ba217
fersyahhh <ferdiapriansyah99@gmail.com>
2026-01-15 14:55:10 +0700

    last update

--- dapps/frontend/my-app/app/providers.tsx
+++ dapps/frontend/my-app/app/providers.tsx
@@ -2,6 +2,8 @@
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { avalancheFuji } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
 const queryClient = new QueryClient();
 const config = createConfig({
   chains: [avalancheFuji],
@@ -12,7 +14,10 @@ const config = createConfig({
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster position="top-right" richColors visibleToasts={1} />
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
